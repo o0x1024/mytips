fafbd971b5839584a002b952d0be4758cdcc7c7e
like <like@adf.com>
2025-08-13 17:48:12 +0800

    udpate:add ollam

--- build_linux.sh
+++ build_linux.sh
@@ -1,8 +1,8 @@
 git add .
-git commit -m "udpate:build linux"
+git commit -m "udpate:add ollam"
 git push 
-git tag -d v1.1.3
-git push origin -d v1.1.3
+git tag -d v1.1.7
+git push origin -d v1.1.7
 
-git tag v1.1.3
-git push origin v1.1.3
+git tag v1.1.7
+git push origin v1.1.7
